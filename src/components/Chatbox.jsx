31c2e64a99697007136a2ec3dac497f027b5b30e
hashir-stack <hashir7ansari@gmail.com>
2026-01-09 20:58:36 +0530

    Make Pages Responsive

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -4,51 +4,65 @@ import { assets } from "../assets/assets";
 import Message from "./Message";
 
 const Chatbox = () => {
-
-  const {selectedChat , theme} = useAppContext();
+  const { selectedChat, theme } = useAppContext();
   const containerRef = useRef(null);
 
-  const[messages,setMessages] = useState([]);
-  const[loading,setLoading] = useState(false);
-  const[prompt,setPrompt] = useState("");
-  const[mode,setMode] = useState('text');
-  const[isPublished,setIsPublished] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [prompt, setPrompt] = useState("");
+  const [mode, setMode] = useState("text");
+  const [isPublished, setIsPublished] = useState(false);
 
-  const onSubmit = async (e) =>{
+  const onSubmit = async (e) => {
     e.preventDefault();
-  }
+  };
 
-  useEffect(()=>{
-    if(selectedChat){
+  useEffect(() => {
+    if (selectedChat) {
       setMessages(selectedChat.messages);
     }
-  },[selectedChat])
+  }, [selectedChat]);
 
   // scroll to very new messages
-  useEffect(()=>{
-    if(containerRef.current){
+  useEffect(() => {
+    if (containerRef.current) {
       containerRef.current.scrollTo({
-        top:containerRef.current.scrollHeight,
-        behavior:"smooth",
-      })
+        top: containerRef.current.scrollHeight,
+        behavior: "smooth",
+      });
     }
-  },[messages]);
+  }, [messages]);
 
   return (
-    <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
+    <div
+      className="
+        flex-1 flex flex-col justify-between 
+        m-3 sm:m-5 md:m-10 xl:mx-30 
+        max-md:mt-14 2xl:pr-40
+      "
+    >
       {/* Chat Messages */}
       <div ref={containerRef} className="flex-1 mb-5 overflow-y-scroll">
         {messages.length === 0 && (
           <div className="h-full flex flex-col items-center justify-center gap-2 text-primary">
-            <img 
-            src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} 
-            alt="Logo" 
-            className="w-full max-w-56 sm:max-w-68"
+            <img
+              src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+              alt="Logo"
+              className="w-full max-w-40 sm:max-w-56 md:max-w-68"
             />
-            <p className="mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white">Ask Me Anything ....</p>
+            <p
+              className="
+                mt-5 text-2xl sm:text-4xl md:text-6xl 
+                text-center text-gray-400 dark:text-white
+              "
+            >
+              Ask Me Anything ....
+            </p>
           </div>
         )}
-        {messages.map((message,index)=> <Message key={index} message={message}/>)}
+        {messages.map((message, index) => (
+          <Message key={index} message={message} />
+        ))}
 
         {/* Loading Animation */}
         {loading && (
@@ -56,48 +70,67 @@ const Chatbox = () => {
             <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
             <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
             <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
-            </div>
+          </div>
         )}
-
       </div>
 
       {/* Publish the image in community */}
-      {mode === 'image' && (
+      {mode === "image" && (
         <label className="inline-flex items-center gap-2 mb-3 text-sm mx-auto">
           <p className="text-xs">Publish Generated Image to Community</p>
-          <input 
-          type="checkbox" 
-          className="cursor-pointer" 
-          checked={isPublished}
-          onChange={(e)=>setIsPublished(e.target.checked)}
+          <input
+            type="checkbox"
+            className="cursor-pointer"
+            checked={isPublished}
+            onChange={(e) => setIsPublished(e.target.checked)}
           />
         </label>
       )}
-      {/* prompt Input Box */}
-      <form 
-      onSubmit={onSubmit}
-      className="bg-primary/20 dark:bg-[#583C79]/30 border border-primary dark:border-[#80609F]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center ">
 
-        <select onChange={(e)=>setMode(e.target.value)} value={mode} className="text-sm pl-3 pr-2 outline-none">
-          <option className="dark:bg-purple-900" value="text">Text</option>
-          <option className="dark:bg-purple-900" value="image">Image</option>
+      {/* Prompt Input Box */}
+      <form
+        onSubmit={onSubmit}
+        className="
+          bg-primary/20 dark:bg-[#583C79]/30 
+          border border-primary dark:border-[#80609F]/30 
+          rounded-full w-full 
+          max-w-md sm:max-w-lg md:max-w-2xl 
+          p-2 sm:p-3 pl-3 sm:pl-4 
+          mx-auto flex gap-2 sm:gap-4 items-center
+        "
+      >
+        <select
+          onChange={(e) => setMode(e.target.value)}
+          value={mode}
+          className="text-sm pl-2 sm:pl-3 pr-2 outline-none"
+        >
+          <option className="dark:bg-purple-900" value="text">
+            Text
+          </option>
+          <option className="dark:bg-purple-900" value="image">
+            Image
+          </option>
         </select>
 
-        <input 
-        type="text" 
-        className="flex-1 w-full text-sm outline-none"
-        placeholder="Type Your Prompt Here ..." 
-        required
-        value={prompt}
-        onChange={(e)=>setPrompt(e.target.value)}
+        <input
+          type="text"
+          className="flex-1 w-full text-sm outline-none"
+          placeholder="Type Your Prompt Here ..."
+          required
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
         />
 
         <button disabled={loading}>
-          <img src={loading ? assets.stop_icon : assets.send_icon} alt="Loading Button"  className="w-8 cursor-pointer"/>
+          <img
+            src={loading ? assets.stop_icon : assets.send_icon}
+            alt="Loading Button"
+            className="w-6 sm:w-8 cursor-pointer"
+          />
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Chatbox;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,107 +3,206 @@ import { useAppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import moment from "moment";
 
-const Sidebar = ({isMenuOpen , setIsMenuOpen}) => {
-
-  const{ chats , setSelectedChat , theme , setTheme , user , navigate}=useAppContext();
-  const [search,setSearch] = useState("");
+const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
+  const { chats, setSelectedChat, theme, setTheme, user, navigate } = useAppContext();
+  const [search, setSearch] = useState("");
 
   return (
-    <div className={`flex flex-col h-screen min-w-72 p-5 dark:bg-linear-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absoulte left-0 z-1 ${!isMenuOpen && 'max-md:-translate-x-full'}`}>
+    <div
+      className={`
+        flex flex-col h-screen 
+        min-w-60 sm:min-w-72 md:min-w-80 
+        p-3 sm:p-5 
+        dark:bg-linear-to-b from-[#242124]/30 to-[#000000]/30 
+        border-r border-[#80609F]/30 
+        backdrop-blur-3xl 
+        transition-all duration-500 
+        max-md:absolute left-0 z-10 
+        ${!isMenuOpen && "max-md:-translate-x-full"}
+      `}
+    >
       {/* Logo */}
-      <img 
-      src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} 
-      alt="logo" 
-      className="w-full max-w-48"
+      <img
+        src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+        alt="logo"
+        className="w-full max-w-36 sm:max-w-48 md:max-w-56"
       />
 
       {/* New Chat button */}
-      <button className="flex justify-center items-center w-full py-2 mt-10 text-white bg-linear-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer">
-        <span className="mr-2 text-2xl">+</span> New Chat
+      <button
+        className="
+          flex justify-center items-center w-full 
+          py-2 mt-6 sm:mt-10 
+          text-white 
+          bg-linear-to-r from-[#A456F7] to-[#3D81F6] 
+          text-xs sm:text-sm md:text-base 
+          rounded-md cursor-pointer
+        "
+      >
+        <span className="mr-2 text-xl sm:text-2xl">+</span> New Chat
       </button>
 
       {/* Search Conversations */}
-      <div className="flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md ">
-          <img src={assets.search_icon} alt="search icon" className="w-4 not-dark:invert"/>
-          <input 
-          type="text" 
+      <div
+        className="
+          flex items-center gap-2 
+          p-2 sm:p-3 mt-4 
+          border border-gray-400 dark:border-white/20 
+          rounded-md
+        "
+      >
+        <img src={assets.search_icon} alt="search icon" className="w-4 not-dark:invert" />
+        <input
+          type="text"
           placeholder="Search Conversations"
-          className="text-xs placeholder:text-gray-400 outline-none"
-          onChange={(e)=>setSearch(e.target.value)}
+          className="text-xs sm:text-sm placeholder:text-gray-400 outline-none flex-1"
+          onChange={(e) => setSearch(e.target.value)}
           value={search}
         />
       </div>
 
       {/* Recent Chats */}
-      {chats.length > 0 && <p className="mt-4 text-sm">Recent Chats</p>}
-      <div className="flex-1 overflow-y-scroll mt-3 text-sm space-y-3">
-        {chats.filter((chat)=> chat.messages[0] ? chat.messages[0]?.content.toLowerCase().includes(search.toLowerCase()) : chat.name.toLowerCase().includes(search.toLowerCase())).map((chat)=>(
-          <div key={chat._id} onClick={()=>{navigate('/');setSelectedChat(chat);setIsMenuOpen(false)}} className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group">
-            <div className="">
-              <p className="truncate w-full">{
-                chat.messages.length > 0 ? chat.messages[0].content.slice(0,32) : chat.name
-              }</p>
-
-              <p className="text-xs text-gray-500 dark:text-[#B1A6C0]">{moment(chat.updatedAt).fromNow()}</p>
+      {chats.length > 0 && <p className="mt-4 text-xs sm:text-sm">Recent Chats</p>}
+      <div className="flex-1 overflow-y-scroll mt-3 text-xs sm:text-sm space-y-3">
+        {chats
+          .filter((chat) =>
+            chat.messages[0]
+              ? chat.messages[0]?.content.toLowerCase().includes(search.toLowerCase())
+              : chat.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((chat) => (
+            <div
+              key={chat._id}
+              onClick={() => {
+                navigate("/");
+                setSelectedChat(chat);
+                setIsMenuOpen(false);
+              }}
+              className="
+                p-2 px-3 sm:px-4 
+                dark:bg-[#57317C]/10 
+                border border-gray-300 dark:border-[#80609F]/15 
+                rounded-md cursor-pointer 
+                flex justify-between group
+              "
+            >
+              <div>
+                <p className="truncate w-full">
+                  {chat.messages.length > 0
+                    ? chat.messages[0].content.slice(0, 32)
+                    : chat.name}
+                </p>
+                <p className="text-[10px] sm:text-xs text-gray-500 dark:text-[#B1A6C0]">
+                  {moment(chat.updatedAt).fromNow()}
+                </p>
+              </div>
+              <img
+                src={assets.bin_icon}
+                alt="Delete"
+                className="hidden group-hover:block w-4 cursor-pointer not-dark:invert"
+              />
             </div>
-
-            <img src={assets.bin_icon} alt="Delete" className="hidden group-hover:block w-4 cursor-pointer not-dark:invert"/>
-          </div>
-        ))
-        }
+          ))}
       </div>
 
       {/* Community Images */}
-      <div onClick={()=>{navigate('/community');setIsMenuOpen(false)}} className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:sacle-103 transition-all">
-        <img src={assets.gallery_icon} alt="Gallery Icon" className="w-4.5 not-dark:invert"/>
-        <div className="flex flex-col text-sm">
+      <div
+        onClick={() => {
+          navigate("/community");
+          setIsMenuOpen(false);
+        }}
+        className="
+          flex items-center gap-2 
+          p-2 sm:p-3 mt-4 
+          border border-gray-300 dark:border-white/15 
+          rounded-md cursor-pointer 
+          hover:scale-105 transition-all
+        "
+      >
+        <img src={assets.gallery_icon} alt="Gallery Icon" className="w-4.5 not-dark:invert" />
+        <div className="flex flex-col text-xs sm:text-sm">
           <p>Community Images</p>
         </div>
       </div>
 
       {/* Credits Purchase option */}
-      <div onClick={()=>{navigate('/credits');setIsMenuOpen(false)}} className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:sacle-103 transition-all">
-        <img src={assets.diamond_icon} alt="Gallery Icon" className="w-4.5 dark:invert"/>
-        <div className="flex flex-col text-sm">
+      <div
+        onClick={() => {
+          navigate("/credits");
+          setIsMenuOpen(false);
+        }}
+        className="
+          flex items-center gap-2 
+          p-2 sm:p-3 mt-4 
+          border border-gray-300 dark:border-white/15 
+          rounded-md cursor-pointer 
+          hover:scale-105 transition-all
+        "
+      >
+        <img src={assets.diamond_icon} alt="Credits Icon" className="w-4.5 dark:invert" />
+        <div className="flex flex-col text-xs sm:text-sm">
           <p>Credits : {user?.credits}</p>
-          <p className="text-xs text-gray-400">Purchase credits to use QuickGPT</p>
+          <p className="text-[10px] sm:text-xs text-gray-400">
+            Purchase credits to use QuickGPT
+          </p>
         </div>
       </div>
 
-        {/* Toggle the themes */}
-      <div className="flex items-center justify-between gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md">
-        <div className="flex items-center gap-2 text-sm">
+      {/* Toggle the themes */}
+      <div
+        className="
+          flex items-center justify-between gap-2 
+          p-2 sm:p-3 mt-4 
+          border border-gray-300 dark:border-white/15 
+          rounded-md
+        "
+      >
+        <div className="flex items-center gap-2 text-xs sm:text-sm">
           <img src={assets.theme_icon} alt="Theme" className="w-4 not-dark:invert" />
           <p>Dark Mode</p>
         </div>
         <label className="relative inline-flex cursor-pointer">
-          <input 
-          type="checkbox" 
-          onChange={()=>setTheme(theme === 'dark' ? 'light' : 'dark')}
-          className="sr-only peer"
-          checked={theme === 'dark'}
+          <input
+            type="checkbox"
+            onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+            className="sr-only peer"
+            checked={theme === "dark"}
           />
-          <div className="w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all">
-          </div>
+          <div className="w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all"></div>
           <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4"></span>
         </label>
       </div>
 
       {/* User Account */}
-
-       <div className="flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group">
-        <img src={assets.user_icon} alt="User Icon" className="w-7 rounded-full"/>
-        <p className="flex-1 text-sm dark:text-primary truncate">
-          {user ? user.name : 'Login Your Account'}
+      <div
+        className="
+          flex items-center gap-3 
+          p-2 sm:p-3 mt-4 
+          border border-gray-300 dark:border-white/15 
+          rounded-md cursor-pointer group
+        "
+      >
+        <img src={assets.user_icon} alt="User Icon" className="w-6 sm:w-7 rounded-full" />
+        <p className="flex-1 text-xs sm:text-sm dark:text-primary truncate">
+          {user ? user.name : "Login Your Account"}
         </p>
+        {user && (
+          <img
+            src={assets.logout_icon}
+            className="h-4 sm:h-5 cursor-pointer hidden not-dark:invert group-hover:block"
+          />
+        )}
+      </div>
 
-        {user && <img src={assets.logout_icon} className="h-5 cursor-pointer hidden not-dark:invert group-hover:block"/>}
-      </div> 
-
-      <img src={assets.close_icon} alt="close icon" className="absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert" onClick={()=>setIsMenuOpen(false)}/>
-
+      {/* Close icon for mobile */}
+      <img
+        src={assets.close_icon}
+        alt="close icon"
+        className="absolute top-3 right-3 w-4 h-4 sm:w-5 sm:h-5 cursor-pointer md:hidden not-dark:invert"
+        onClick={() => setIsMenuOpen(false)}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;
