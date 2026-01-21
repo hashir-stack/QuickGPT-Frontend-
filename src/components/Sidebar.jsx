fae4c648617e177ef311e78d57ad17c9d5a6a5be
hashir-stack <hashir7ansari@gmail.com>
2026-01-21 23:37:30 +0530

    Fixed the Logou Button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -217,7 +217,7 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
           <img
             onClick={logout}
             src={assets.logout_icon}
-            className="h-4 sm:h-5 cursor-pointer hidden not-dark:invert group-hover:block"
+            className="h-4 sm:h-5 cursor-pointer not-dark:invert group-hover:block"
           />
         )}
       </div>
